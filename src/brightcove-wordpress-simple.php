1354852d8c446b29f99dc61bea64ddc037fb8e96
mister-ben <bmclifford@gmail.com>
2014-06-10 16:03:46 +0100

    Use DOMDocument::saveXML for PHP < 5.3.6
    DOMDocument::saveXML instead of DOMDocument::saveHTML

--- src/brightcove-wordpress-simple.php
+++ src/brightcove-wordpress-simple.php
@@ -1,11 +1,11 @@
 <?php
 /**
  * Plugin Name: Simple Brightcove Player Embed
- * Plugin URI: https://github.com/Brightcodes/Brightcove-Wordpress
+ * Plugin URI: http://wordpress.org/plugins/simple-brightcove-player-embed/
  * Description: This allows a Brighcove Video Cloud video to be easily embedded by the URL you get from the studio. It does not need a Video Cloud Media API key, so works with any Brightcove account edition.
- * Version: 0.6
+ * Version: 0.8
  * Author: mister-ben
- * Author URI: https://github.com/Brightcodes/Brightcove-Wordpress
+ * Author URI: http://wordpress.org/plugins/simple-brightcove-player-embed/
  * License:GPL2
  * Usage: [bc]http://bcove.me/bbnvrhso[/bc]
  *      [bc width="480" height="270"]http://bcove.me/bbnvrhso[/bc]
@@ -70,12 +70,19 @@ class BC_Shortcode {
 
     // Get video ID from link, if present
     $videoId = null;
-    foreach ($http_response_header as $header) {
-      if (substr($header, 0, 10) == 'Location: ') {
-        $location = explode(': ', $header);
-        parse_str(parse_url($location[1], PHP_URL_QUERY),$args);
-        if (isset($args['bctid'])) {
-          $videoId = $args['bctid'];
+
+    // Try to get video ID from URL, then header
+    parse_str(parse_url($content, PHP_URL_QUERY),$args);
+    if (isset($args['bctid'])) {
+      $videoId = $args['bctid'];
+    } else {
+      foreach ($http_response_header as $header) {
+        if (substr($header, 0, 10) == 'Location: ') {
+          $location = explode(': ', $header);
+          parse_str(parse_url($location[1], PHP_URL_QUERY),$args);
+          if (isset($args['bctid'])) {
+            $videoId = $args['bctid'];
+          }
         }
       }
     }
@@ -147,11 +154,11 @@ class BC_Shortcode {
     $object->setAttribute("id",$expid);
 
     // IE8 fix — some themes give <object>s max-width:100%, which collapses the player on IE8
-    //$ie8Style = '<!--[if IE 8]><style type="text/css">.BrightcoveExperience {max-width:none;}</style><![endif]-->';
+    $ie8Style = '<!--[if IE 8]><style type="text/css">.BrightcoveExperience {max-width:none;}</style><![endif]-->';
 
     // Make sure the script gets written, and return HTML
     self::$add_script = true;
-    return "<!-- Brightcove Video -->\n" . self::$dom->saveHTML($object) . "\n" . $ie8Style;
+    return "<!-- Brightcove Video -->\n" . self::$dom->saveXML($object) . "\n" . $ie8Style;
   }
 
   static function register_script() {
